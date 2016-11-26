edf9078343c9816145dfbeb8391d9b0860d882c3
michael <michaeldinnall@gmail.com>
2016-11-26 14:27:30 -0500

    updated App file

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+          
+          <h1>Waiting for build out.....</h1>
+          
+      </div>
+    )
+  }
+})
+
+module.exports = App;
